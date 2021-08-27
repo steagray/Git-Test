b8d79ead551cc52172f8b2a79e08e207d62a78e0
steagray <steagray@ut.utm.edu>
2021-08-26 19:40:53 -0500

    The world has been unhello'd and replaced with a number guessing game.

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,7 +10,33 @@ namespace GitTest
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Random rand = new Random();
+			int maxRandom = 100; //the highest number that can be selected
+			int minRandom = 0; //the lowest number that can be selected
+			int randNum = rand.Next(minRandom, maxRandom + 1); //generates a random integer from minRandom to maxRandom
+			int userGuess = minRandom - 1; //initializes userGuess to an answer that is impossible to be correct
+			Console.WriteLine(String.Format("Random Number Generated from {0} to {1}!", minRandom, maxRandom));
+			int attemptCount = 0;
+
+			while (userGuess != randNum)
+			{
+				Console.Write("Select a number: ");
+				userGuess = Convert.ToInt32(Console.ReadLine());
+
+				if (userGuess > randNum)
+				{
+					Console.WriteLine(String.Format("{0} is too high!", userGuess));
+				}
+				if (userGuess < randNum)
+				{
+					Console.WriteLine(String.Format("{0} is too low!", userGuess));
+				}
+
+				attemptCount++;
+			}
+
+			Console.WriteLine(String.Format("It took you {0} attempts!", attemptCount));
+
 		}
 	}
 }
