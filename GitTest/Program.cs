7bbfbd34d749e5e240a73a4da8541d13fab90f93
steagray <steagray@ut.utm.edu>
2021-08-26 19:52:09 -0500

    user input added

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -11,8 +11,19 @@ namespace GitTest
 		static void Main(string[] args)
 		{
 			Random rand = new Random();
-			int maxRandom = 100; //the highest number that can be selected
-			int minRandom = 0; //the lowest number that can be selected
+			Console.Write("Select a maximum number: ");
+			int maxRandom = Convert.ToInt32(Console.ReadLine()); //the highest number that can be selected
+			
+			int minRandom; //the lowest number that can be selected
+			do
+			{
+				Console.Write("Select a minimum number: ");
+				minRandom = Convert.ToInt32(Console.ReadLine());
+				if (minRandom > maxRandom)
+				{
+					Console.WriteLine("Minimum number must be less than or equal to the maximum number");
+				}
+			} while (minRandom > maxRandom); //guarantees that the min is lower than the max
 			int randNum = rand.Next(minRandom, maxRandom + 1); //generates a random integer from minRandom to maxRandom
 			int userGuess = minRandom - 1; //initializes userGuess to an answer that is impossible to be correct
 			Console.WriteLine(String.Format("Random Number Generated from {0} to {1}!", minRandom, maxRandom));
